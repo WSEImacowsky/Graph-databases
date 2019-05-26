4380a5ddd709183e21a5a175cc056214abbd5c96
Mleczko Maciej <maciej.mleczko@microsoft.wsei.edu.pl>
2019-05-26 17:50:59 +0200

    added two projects

---
+++ Algotihms and Data Structtures/STPAR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STPAR
+{
+    class Program
+    {
+        static Queue<int> queIn = new Queue<int>();
+        static Queue<int> queOut = new Queue<int>();
+        static Stack<int> helpStack = new Stack<int>();
+
+        static Queue<int> ReadData()
+        {
+            int t = Int32.Parse(Console.ReadLine());
+            if(t == 0) return null;
+
+            int[] tab = Array.ConvertAll(
+                Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries), int.Parse
+            );
+
+            return new Queue<int>(tab);
+        }
+
+
+        static void Main(string[] args)
+        {
+            while((queIn = ReadData()) != null)
+            {
+
+            }
+        }
+    }
+}
